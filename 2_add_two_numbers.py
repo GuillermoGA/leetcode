d851014651f4e5c3d4b9454e27de00284ff5f621
Guillermo Gilpérez Alcázar <ggilperezalcazar@gmail.com>
2022-04-28 20:54:48 +0200

    problem 2
    https://leetcode.com/problems/add-two-numbers/

---
+++ 2_add_two_numbers.py
@@ -0,0 +1,59 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    """
+    >>> obj = Solution()
+    >>> l1 = ListNode(2, ListNode(4, ListNode(3)))
+    >>> l2 = ListNode(5, ListNode(6, ListNode(4)))
+    >>> obj.addTwoNumbers(l1 = l1, l2 = l2)
+    [7, 0, 8]
+    >>>> obj.addTwoNumbers(l1 = [0], l2 = [0])
+    [0]
+    >>>> obj.addTwoNumbers(l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9])
+    [8, 9, 9, 9, 0, 0, 0, 1]
+    """
+
+    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+        num1 = []
+        while(l1):
+            num1.append(l1.val)
+            l1 = l1.next
+
+        num2 = []
+        while(l2):
+            num2.append(l2.val)
+            l2 = l2.next
+
+        num1 = int("".join(list(map(lambda x: str(x), num1[::-1]))))
+        num2 = int("".join(list(map(lambda x: str(x), num2[::-1]))))
+
+        sum_num = num1 + num2
+
+        sum_num = str(sum_num)[::-1]
+
+        linked_list = None
+        prev = None
+        for digit in sum_num:
+            digit = int(digit)
+            if not prev:
+                linked_list = ListNode(digit)
+                prev = linked_list
+            else:
+                prev.next = ListNode(digit)
+                prev = prev.next
+                
+        return linked_list
+
+
+if __name__ == "__main__":
+    # import doctest
+    # doctest.testmod()
+    l1 = ListNode(2, ListNode(4, ListNode(3)))
+    l2 = ListNode(5, ListNode(6, ListNode(4)))
+    obj = Solution()
+    print(obj.addTwoNumbers(l1=l1, l2=l2))
