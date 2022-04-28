29a3226b8615efe6c146abea84a292000349e42a
Guillermo Gilpérez Alcázar <ggilperezalcazar@gmail.com>
2022-04-28 23:23:16 +0200

    problem 4

---
+++ 4_median-of-two-sorted-arrays.py
@@ -0,0 +1,25 @@
+from heapq import merge
+
+
+class Solution:
+    """
+    >>> obj = Solution()
+    >>> obj.findMedianSortedArrays(nums1 = [1,3], nums2 = [2])
+    2
+    >>> obj.findMedianSortedArrays(nums1 = [1,2], nums2 = [3,4])
+    2.5
+    """
+
+    def findMedianSortedArrays(self, nums1: list[int], nums2: list[int]) -> float:
+        merged_sorted = nums1 + nums2
+        merged_sorted.sort()
+        if len(merged_sorted) % 2 == 1:
+            return merged_sorted[int(len(merged_sorted)/2)]
+
+        half = int(len(merged_sorted)/2)
+        return (merged_sorted[half]+merged_sorted[half-1])/2
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
