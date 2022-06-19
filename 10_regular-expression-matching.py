ed1975f23155f3bc57371d08d32cdea71d2a5119
Guillermo Gilpérez Alcázar <ggilperezalcazar@gmail.com>
2022-06-19 22:43:14 +0200

    problem 10

---
+++ 10_regular-expression-matching.py
@@ -0,0 +1,28 @@
+class Solution:
+    """
+    >>> obj = Solution()
+    >>> obj.isMatch("aa", "a")
+    False
+    >>> obj.isMatch("aa", "a*")
+    True
+    >>> obj.isMatch("ab", ".*")
+    True
+    >>> obj.isMatch("aaa", "a*a")
+    True
+    """
+        
+    def isMatch(self, text, pattern):
+        if not pattern:
+            return not text
+
+        first_match = bool(text) and pattern[0] in {text[0], '.'}
+
+        if len(pattern) >= 2 and pattern[1] == '*':
+            return (self.isMatch(text, pattern[2:]) or
+                    first_match and self.isMatch(text[1:], pattern))
+        else:
+            return first_match and self.isMatch(text[1:], pattern[1:])
+        
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
