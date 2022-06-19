a4eba9156d2188755df9fefd20e4ea1a9fd4e0c3
Guillermo Gilpérez Alcázar <ggilperezalcazar@gmail.com>
2022-06-19 21:14:09 +0200

    format code

--- 7_reverse-integer.py
+++ 7_reverse-integer.py
@@ -8,29 +8,28 @@ class Solution:
     >>> obj.reverse(120)
     21
     """
-    def reverse(self, x: int) -> int:     
+
+    def reverse(self, x: int) -> int:
         str_x = str(x)
         negative = str_x[0] == "-"
-        
+
         if negative:
             str_x = str_x[1:]
-            
+
         str_x = str_x[::-1]
-        
+
         if negative:
             str_x = "-" + str_x
-            
+
         reversed_x = int(str_x)
-    
+
         # Return 0 for non 32 bit integer
         if not -2**31 <= reversed_x <= 2**31-1:
             return 0
-        
+
         return reversed_x
-        
+
 
 if __name__ == "__main__":
     import doctest
     doctest.testmod()
-
-        
